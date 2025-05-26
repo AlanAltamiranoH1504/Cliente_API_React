bc0680456f7f5f33e2f290516b9238cd3ed068aa
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-05-26 12:43:05 -0600

    feat: add basic body of components for customers, products and orders - add react router dom - add axios configuration for backend requests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React, {Fragment} from "react";
+
+//Routing
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+//Layout
+import Header from "./components/layout/Header";
+import Navegacion from "./components/layout/Navegacion";
+
+//Clientes
+import Cliente from "./components/clientes/Cliente";
+
+//Productos
+import Productos from "./components/productos/Productos";
+
+//Pedidos
+import Pedidos from "./components/pedidos/Pedidos";
+
 function App() {
     return (
-        <h1>Hola</h1>
+        <Router>
+            <Fragment>
+                <Header/>
+
+                <div className="grid contenedor contenido-principal">
+                    <Navegacion/>
+                    <main className="caja-contenido col-9">
+                        <Routes>
+                            <Route exact path="/" element={< Cliente />}></Route>
+                            <Route exact path="/productos" element={<Productos/>}></Route>
+                            <Route exact path="/pedidos" element={<Pedidos />}></Route>
+                        </Routes>
+                    </main>
+                </div>
+            </Fragment>
+        </Router>
     );
 }
-
 
 
 export default App;
---
+++ src/components/clientes/Cliente.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Cliente = () => {
+    return (
+        <h2>Clientes</h2>
+    );
+}
+export default Cliente;
